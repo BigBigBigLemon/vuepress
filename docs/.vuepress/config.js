1fbfce5857f00d02973fd6bae35e0e5e125490fa
Monster <wangshu-134@163.com>
2021-07-07 05:18:05 +0800

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: "Monster's Blog",
+  description: "welcome",
+  base: "/",
+  themeConfig: {
+    sidebar: "auto",
+    nav: [
+      { text: "Guide", link: "/guide/" },
+      { text: "GitHub", link: "/git/" },
+      { text: "InterView", link: "/interview/" },
+      { text: "浏览器缓存", link: "/cache/" },
+      { text: "Monster", link: "https://github.com/BigBigBigLemon" }
+    ],
+    lastUpdated: "Last Updated"
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+lang: zh-CN
+heroText: 人之好我，示我周行。
+heroImage: /monster.jpg
+actionText: 开始 →
+actionLink: /git/
+features:
+- title: A Blog
+  details: 专注写作前端博客
+- title: A Diary
+  details: 记录成长经历，日常所得
+- title: To You
+  details: 愿有人与你立黄昏，围炉笑问粥可温
+footer: MIT Licensed | Copyright © 2020-present Monster
+---
