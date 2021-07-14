6b7f4e49055dbb702731b5e20d765cadc3b6f3cd
Monster <wangshu-134@163.com>
2021-07-15 03:22:55 +0800

    添加插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,12 @@
 module.exports = {
   title: "Monster's Blog",
-  // description: 'welcome',
+  description: 'welcome',
   head: [['link', { rel: 'icon', href: '/gh-page.ico' }]],
   base: '/',
   themeConfig: {
     sidebar: 'auto',
-    nav: [
-      { text: 'Guide', link: '/guide/' },
-      { text: 'GitHub', link: '/git/' },
-      { text: 'InterView', link: '/interview/' },
-      { text: '浏览器缓存', link: '/cache/' },
-      { text: 'Monster', link: 'https://github.com/BigBigBigLemon' },
-    ],
+    nav: require('./nav/nav'),
     lastUpdated: 'Last Updated',
   },
+  plugins: ['@vuepress/back-to-top']
 };
---
+++ docs/.vuepress/nav/nav.js
@@ -0,0 +1,21 @@
+module.exports = [
+  {
+    text: '主页',
+    link: '/',
+  },
+  {
+    text: '目录索引',
+    items: [
+      {
+        text: '面试',
+        link: '/interview/',
+      },
+      {
+        text: 'GitHub',
+        link: '/git/',
+      },
+    ],
+  },
+  { text: '浏览器缓存', link: '/cache/' },
+  { text: 'Monster', link: 'https://github.com/BigBigBigLemon' },
+];
